d0a3f2b3485e496bffd38131b3c735829870ebd9
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-03-17 09:28:21 -0300

    refactor of the class InputArray

--- array/InputArray.java
+++ array/InputArray.java
@@ -1,24 +1,24 @@
 package array;
-import java.util.Scanner;
+import java.io.IOException;
+import input.Input;
 
  class InputArray {
- public static void main(String args[]) {
+ public static void main(String args[]) throws IOException  {
     String arrayInput="";
     System.out.println("Enter the length of array ");
-    try(Scanner in = new Scanner(System.in)){
-        int lengthArray = in.nextInt();
-        if(lengthArray > 10 || lengthArray < 1){
-            System.out.println("Number is invalid");
-        }else{
-            int[] listNumbers = new int[lengthArray];
-            for(int i =0; i< listNumbers.length; i++){
-                System.out.println("Enter the number " + ( i + 1));
-                listNumbers[i] = in.nextInt();
-                arrayInput = arrayInput + " " + "[" + listNumbers[i] + "]";
+    int lengthArray = Input.main();
+    if(lengthArray > 10 || lengthArray < 1){
+        System.out.println("Number is invalid");
+    }else{
+        int[] listNumbers = new int[lengthArray];
+        for(int i =0; i< listNumbers.length; i++){
+            System.out.println("Enter the number " + ( i + 1));
+            listNumbers[i] = Input.main();
+            arrayInput = arrayInput + " " + "[" + listNumbers[i] + "]";
             }
-            System.out.println(arrayInput);
+        System.out.println(arrayInput);
         }
-    }    
+
  }   
 
 }
